e0d83a75a0363caa306fb48e712b93bafe08a748
GugaRosa456 <rosaguga07@gmail.com>
2025-11-25 11:15:58 -0300

    login funcionando e com listagem

--- Programa-o-pra-web-master/conexao.php
+++ Programa-o-pra-web-master/conexao.php
@@ -18,19 +18,19 @@ function cadastra_usuario($nome, $login, $senha){
     return $stmt->execute();
 }
 //exemplo cadastro
-cadastra_usuario("João Silva", "joao", "12345");
+//cadastra_usuario("João Silva", "joao", "12345");
 
 function delete_usuario($id){
-$con= connecta_bd();
-$stmt =$con->prepare("DELETE FROM usuarios WHERE id = :id");
-$stmt->bindparam(':id', $id);
-return $stmt->execute();
+        $con= connecta_bd();
+        $stmt =$con->prepare("DELETE FROM usuarios WHERE id = :id");
+        $stmt->bindparam(':id', $id);
+        return $stmt->execute();
 }
 //delete_usuario(1); 
 function update_usuario ($id, $nome, $login, $senha){
     $con= connecta_bd();
-    $stmt=$con->prepare("UPDATE usuarios SET nome = :nome, login = :login, senha = :senha WHERE id = :id");
-    $stmt->bindparam(':id', $id);
+   $stmt=$con->prepare("UPDATE usuarios SET nome = :nome, login = :login, senha = :senha WHERE id = :id");
+  $stmt->bindparam(':id', $id);
     $stmt->bindparam(':nome', $nome);
     $stmt->bindparam(':login', $login);
     $stmt->bindparam(':senha', $senha);
@@ -40,11 +40,19 @@ function get_usuario($id){
     $con= connecta_bd();
     $stmt=$con->prepare("SELECT * FROM usuarios WHERE id = :id");
     $stmt->bindparam(':id', $id);
+   $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+function get_usuarios(){
+    $con= connecta_bd();
+    $stmt=$con->prepare("SELECT * FROM usuarios");
     $stmt->execute();
-    return $stmt->fetch(PDO::FETCH_ASSOC);
+    //echo var_dump ($stmt->fetchAll(PDO::FETCH_ASSOC));
+    
+     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 //echo var_dump(get_usuario(2));
-echo "<pre>";
-print_r(var_dump(get_usuarios()));
-echo "</pre>";
+//echo "<pre>";
+//print_r(var_dump(get_usuarios()));
+//echo "</pre>";
 ?>
