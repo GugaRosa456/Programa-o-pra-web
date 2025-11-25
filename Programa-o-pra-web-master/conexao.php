b5be2c05ea5a5ce4b222c81a5eb84827037b613f
GugaRosa456 <rosaguga07@gmail.com>
2025-11-25 08:44:43 -0300

    php , banco funcionando

--- Programa-o-pra-web-master/conexao.php
+++ Programa-o-pra-web-master/conexao.php
@@ -1,23 +1,33 @@
 <?php 
 function connecta_bd(){
-    $servername = "localhost";
-    $username = "aluno";
-    $password = "aluno";
-    $dbname = "webti";
+    $servername = "localhost:3307";
+    $username = "root";
+    $password = "";
+    $dbname = "bd";
     // Criar conexao
-    return new PDO("mysql:host=$servername;dbname=$dbname");
+    return new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 }
 //connecta_bd();
 
-function cadastra_usuario($nome, $email, $login, $senha){
-    $con= connecta_db();
-    $smtt =$con->prepare("INSERT INTO usuarios (email, login, senha) VALUES (:nome, :email :login, :senha)");
+function cadastra_usuario($nome, $login, $senha){
+    $con= connecta_bd();
+    $stmt =$con->prepare("INSERT INTO usuarios ( nome, login, senha) VALUES (:nome, :login, :senha)");
     $stmt->bindParam(':nome', $nome);
-    $stmt->bindParam(':nome', $email);
-    $stmt->bindParam(':nome', $login);
-    $stmt->bindParam(':nome', $senha);
+    $stmt->bindParam(':login', $login);
+    $stmt->bindParam(':senha', $senha);
     return $stmt->execute();
 }
 //exemplo cadastro
 cadastra_usuario("João Silva", "joao", "12345");
+
+function delete_usuario($id){
+$con= connecta_bd();
+$stmt =$con->prepare("DELETE FROM usuarios WHERE id = :id");
+$stmt->bindParam(':id', $id);
+return $stmt->execute();
+}
+//delete_usuario(1); 
+function update_usuario ($id, $nome, $login, $senha){
+    $con= connecta_bd();
+}
 ?>
