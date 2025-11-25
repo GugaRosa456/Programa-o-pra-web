02f36ff3a95e7057f0a2926d34f5f8e59d9f5af9
GugaRosa456 <rosaguga07@gmail.com>
2025-11-25 09:07:16 -0300

    get_usuario e printar usuarios no php

--- Programa-o-pra-web-master/conexao.php
+++ Programa-o-pra-web-master/conexao.php
@@ -23,11 +23,28 @@ cadastra_usuario("João Silva", "joao", "12345");
 function delete_usuario($id){
 $con= connecta_bd();
 $stmt =$con->prepare("DELETE FROM usuarios WHERE id = :id");
-$stmt->bindParam(':id', $id);
+$stmt->bindparam(':id', $id);
 return $stmt->execute();
 }
 //delete_usuario(1); 
 function update_usuario ($id, $nome, $login, $senha){
     $con= connecta_bd();
+    $stmt=$con->prepare("UPDATE usuarios SET nome = :nome, login = :login, senha = :senha WHERE id = :id");
+    $stmt->bindparam(':id', $id);
+    $stmt->bindparam(':nome', $nome);
+    $stmt->bindparam(':login', $login);
+    $stmt->bindparam(':senha', $senha);
+    return $stmt->execute();
 }
+function get_usuario($id){
+    $con= connecta_bd();
+    $stmt=$con->prepare("SELECT * FROM usuarios WHERE id = :id");
+    $stmt->bindparam(':id', $id);
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+//echo var_dump(get_usuario(2));
+echo "<pre>";
+print_r(var_dump(get_usuarios()));
+echo "</pre>";
 ?>
